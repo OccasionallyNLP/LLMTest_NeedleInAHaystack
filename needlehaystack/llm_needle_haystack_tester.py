b66aa52d63fb0125a4a26d9b6da242feab511e99
OccasionallyNLP <tillcry8@gmail.com>
2025-06-30 21:34:36 +0900

    h

--- needlehaystack/llm_needle_haystack_tester.py
+++ needlehaystack/llm_needle_haystack_tester.py
@@ -194,7 +194,7 @@ class LLMNeedleHaystackTester:
             print (f"Response: {response}\n")
 
         context_file_location = f'{self.model_name.replace(".", "_")}_len_{context_length}_depth_{int(depth_percent*100)}'
-
+        context_file_location = context_file_location.replace('/','_')
         if self.save_contexts:
             results['file_name'] = context_file_location
 
